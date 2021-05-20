0c472325b7e2defa4ead647bb874e40be82ca761
Hannah Gunn <hannah.gunn@vodafone.com>
2021-05-20 11:35:11 +0100

    feat: ✨ Added snowflakes to the top of the webpage

--- src/content.ts
+++ src/content.ts
@@ -1,2 +1,17 @@
 // This file is injected as a content script
-console.log("Hello from content script!")
+
+const body = document.getElementsByTagName("body")
+
+const snowflakesContainer = document.createElement("div")
+snowflakesContainer.className = "snowflakes"
+snowflakesContainer.setAttribute("aria-hidden", "true")
+
+const snowflake = document.createElement("div")
+snowflake.className = "snowflake"
+snowflake.innerHTML = "❆"
+
+for (let i = 0; i < 12; i++){
+    snowflakesContainer.appendChild(snowflake.cloneNode(true))
+}
+
+body[0]?.prepend(snowflakesContainer)
